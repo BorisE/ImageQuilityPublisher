43ca9fb554cd96475e39fb170302b3cd2c74cf1b
Emchenko Boris <Emchenko Boris@DELLXPS13>
2018-01-01 13:37:44 +0300

    0.1b

--- ImageQuilityPublisher/Properties/AssemblyInfo.cs
+++ ImageQuilityPublisher/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageQuilityPublisher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Publish FITS quality estimates to web")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageQuilityPublisher")]
